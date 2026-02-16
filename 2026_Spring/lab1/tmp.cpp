87f64c0406a60d3f816d964e639dd04dcf3eaf73
Cameron Thomas <cameron102601@gmail.com>
2026-02-16 11:15:31 -0600

    Putting my work so far into GIT

--- 2026_Spring/lab1/tmp.cpp
+++ 2026_Spring/lab1/tmp.cpp
@@ -1,3 +1,6 @@
+//Save of oldest best top.cpp
+// 51499 cycles * 6.269 CP = 0.3228ms
+
 #include "dcl.h"
 
 // Baseline implementation for HLS.
@@ -9,68 +12,67 @@ void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
 #pragma HLS interface s_axilite port=return
 
     // On-chip buffers for A_DRAM and C_DRAM
+    data_t A[N_ROWS][N_COLS];
     data_t C[N_ROWS][N_COLS];
-#pragma HLS array_partition variable=C dim=2 factor=8 cyclic
 
-    data_t A_buf[2][N_COLS];
-#pragma HLS array_partition variable=A_buf dim=2 factor=8 cyclic
+#pragma HLS array_partition variable=A dim=2 factor=8 cyclic
+#pragma HLS array_partition variable=C dim=2 factor=8 cyclic
 
     // Storage for normalized A
     data_t B[N_ROWS][N_COLS];
+
 #pragma HLS array_partition variable=B dim=2 factor=8 cyclic
+
+    for (int i = 0; i < N_ROWS; i++) {
+#pragma HLS pipeline II=1
+        for (int j = 0; j < N_COLS; j++) {
+            A[i][j] = A_DRAM[i][j];
+        }
+    }
 
     // Storage for sums
     data_t col_sum[N_COLS];
+    data_t row_sum[N_ROWS];
 #pragma HLS array_partition variable=col_sum dim=1 complete
+#pragma HLS array_partition variable=row_sum dim=1 complete
 
     // Init sums to 0
     for (int j = 0; j < N_COLS; j++) {
 #pragma HLS unroll
         col_sum[j] = 0;
     }
-
-    // Prime the pipeline: read row 0
-    for (int j = 0; j < N_COLS; j++) {
-#pragma HLS pipeline II=1
-        A_buf[0][j] = A_DRAM[0][j];
+    for (int i = 0; i < N_ROWS; i++) {
+#pragma HLS unroll
+        row_sum[i] = 0;
     }
 
-    for (int i = 1; i <= N_ROWS; i++) {
-
-        int ping = (i - 1) & 1;
-        int pong = i & 1;
-
-        // --- READ NEXT ROW ---
-        if (i < N_ROWS) {
-            for (int j = 0; j < N_COLS; j++) {
-#pragma HLS pipeline II=1
-                A_buf[pong][j] = A_DRAM[i][j];
+    // Phase 1: Row-wise normalization
+    for (int i = 0; i < N_ROWS; i++) {
+        // Compute row sum
+        for (int j = 0; j < N_COLS; j+=8) {
+#pragma HLS pipeline
+            data_t local_sum = 0;
+            for (int jj = 0; jj < 8; jj++) {
+#pragma HLS unroll
+                local_sum += A[i][j+jj];
             }
+            row_sum[i] += local_sum;
         }
 
-        // --- COMPUTE CURRENT ROW ---
-        data_t row_sum = 0;
+        // Avoid division by zero, add small bias
+        data_t denom = row_sum[i] + (data_t)1.0;
+
+        // Normalize each element in the row and add to col_sum
         for (int j = 0; j < N_COLS; j+=8) {
 #pragma HLS pipeline
             for (int jj = 0; jj < 8; jj++) {
 #pragma HLS unroll
-                row_sum += A_buf[ping][j+jj];
-            }
-        }
-
-        data_t denom = row_sum + (data_t)1.0;
-
-        for (int j = 0; j < N_COLS; j+=8) {
-#pragma HLS pipeline
-            for (int jj = 0; jj < 8; jj++) {
-#pragma HLS unroll
-                data_t tmp = A_buf[ping][j+jj] / denom;
-                B[i-1][j+jj] = tmp;
+                data_t tmp = A[i][j+jj] / denom;
+                B[i][j+jj] = tmp;
                 col_sum[j+jj] += tmp;
             }
         }
     }
-
 
     // Phase 2: Column-wise scaling
     for (int j = 0; j < N_COLS; j++) {
@@ -87,9 +89,8 @@ void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
         }
     }
     
-    // Output to DRAM
     for (int i = 0; i < N_ROWS; i++) {
-#pragma HLS pipeline
+#pragma HLS pipeline II=1
         for (int j = 0; j < N_COLS; j++) {
             C_DRAM[i][j] = C[i][j];
         }
